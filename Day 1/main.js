13536c25c1ec98507e360aa63eb24ddeaa8747a0
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-10-17 00:49:59 +0300

    Creating Task2

---
+++ Day 1/main.js
@@ -0,0 +1,21 @@
+document.write("<h1>Hello <span>Page</span></h1>");
+
+console.log("Hello from JS");
+
+
+//var 
+var name = "ahmed";
+console.log(name); //Output: ahmed
+name = "omar";
+console.log(name); //Output: omar
+
+//let 
+let age = 20;
+console.log(age); //Output: 20
+age = 23;
+console.log(age); //Output: 23
+
+//const 
+const birthYear = 2004;
+console.log(birthYear); //Output: 2004;
+
