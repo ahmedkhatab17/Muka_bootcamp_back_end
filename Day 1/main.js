baffbeb31d31b3f0fba1634246f9c1a1fe3d2268
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-10-17 01:08:39 +0300

    Creating Task3

--- Day 1/main.js
+++ Day 1/main.js
@@ -2,7 +2,7 @@ document.write("<h1>Hello <span>Page</span></h1>");
 
 console.log("Hello from JS");
 
-
+//Task 2
 //var 
 var name = "ahmed";
 console.log(name); //Output: ahmed
@@ -19,3 +19,15 @@ console.log(age); //Output: 23
 const birthYear = 2004;
 console.log(birthYear); //Output: 2004;
 
+//Task 3
+let result1 = "5" + 2; 
+console.log(result1); //Output: "52"
+
+let result2 = "5" - 2; 
+console.log(result2); //git Output: 3 (???)
+
+let result3 = true /* = 1 */ + 1;
+console.log(result3); //Output: 2
+
+let result4 = null /* = 0 */ + 5;
+console.log(result4); //Output: 5
