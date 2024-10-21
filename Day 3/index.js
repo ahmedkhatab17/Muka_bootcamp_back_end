523db6dbe250b71878579c642e47d6a7f0430dd3
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-10-21 23:23:52 +0300

    Feat task 1

---
+++ Day 3/index.js
@@ -0,0 +1,13 @@
+/////////// Task 1 ////////////////
+function checkAge(age) {
+    if (age >= 18) {
+        console.log("You are eligible to vote");
+    } else {
+        console.log("You are not eligible to vote");
+    }
+}
+
+checkAge(20);
+checkAge(15);
+
+/////////// Task 2 ////////////////
