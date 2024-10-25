a0ed1cfc7b77b0d45a6e43d4e9b829dff542e8b9
ahmed khatab <ahmedkhatab9631@gmail.com>
2024-10-25 14:33:00 +0300

    Feat Day 6

---
+++ Day 6/index.js
@@ -0,0 +1,57 @@
+///////////////////// Day 6 /////////////////////////
+
+////////////// Task 1 /////////////////
+
+function getListStudents() {
+    let students = [
+        { id: 1, firstName: "ahmed", location: "cairo" },
+        { id: 2, firstName: "omar", location: "cairo" },
+        { id: 3, firstName: "alaa", location: "alex" }
+    ];
+    return students;
+}
+console.log(getListStudents());
+
+////////////// Task 2 /////////////////
+
+function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    return students.map(student => student.id);
+}
+console.log(getListStudentIds(getListStudents()));
+
+////////////// Task 3 /////////////////
+
+function getStudentsByLocation(students, city) {
+    return students.filter(student => student.location === city);
+}
+console.log(getStudentsByLocation(getListStudents(), "alex"));
+
+////////////// Task 4, 5 /////////////////
+
+// i can't do them need explaintion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
